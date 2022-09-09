b2fb5dedb368c61e017cb9cfe7ebd74f2b072b50
qzj995@outlook.com <qzj995@outlook.com>
2022-09-09 16:24:56 +0800

    update database

--- orm.py
+++ orm.py
@@ -8,14 +8,21 @@ def connect_db():
     global conn, c
     conn = sqlite3.connect("data.db")
     c = conn.cursor()
-    sql_create_tables = '''CREATE TABLE IF NOT EXISTS FORM (
+    sql_create_table_info = '''CREATE TABLE IF NOT EXISTS VOTE_INFO (
         ID INTEGER PRIMARY KEY NOT NULL,
-        NAME TEXT NOT NULL,
-        OPTIONS TEXT,
-        COMMENT TEXT,
+        FACTORY TEXT NOT NULL,
+        UID TEXT,
         TIME TEXT 
     )'''
-    c.execute(sql_create_tables)
+    sql_create_table_detail = '''CREATE TABLE IF NOT EXISTS VOTE_DETAIL(
+        ID INTEGER PRIMARY KEY NOT NULL,
+        UID TEXT,
+        PRODUCT TEXT,
+        CHOICE INTEGER,
+        NOTE TEXT
+    )'''
+    c.execute(sql_create_table_info)
+    c.execute(sql_create_table_detail)
     conn.commit()
 
 
@@ -24,7 +31,6 @@ def insert_db(item):
     now_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
     sql_insert_item = f'INSERT INTO FORM (NAME, OPTIONS, COMMENT, TIME) VALUES ("{item.name}", "{item.options}", ' \
                       f'"{item.comment}", "{now_time}")'
-    print(sql_insert_item)
     try:
         c.execute(sql_insert_item)
         conn.commit()
